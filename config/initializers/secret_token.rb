88babfa36a9c618b93737b5ae744a58918852c5d
lhuang42 <lhuang42@gmail.com>
2014-01-22 00:53:52 -0500

    Since this is just an assignment, it's okay to leave secret_token.rb in here... also this means it'll actually work.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geneatree::Application.config.secret_key_base = '21455d140d2f3ae65d611ade8e7e6345a3e631c4501622309fce2b39056568b87aa4c6bde6b50a5f6125841aa860e53aa8b788516dbc2c34cd88d17db06b5fd4'
